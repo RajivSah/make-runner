34e9b0fd45d81429d247b5643b3adf2af6921023
Rajiv Sah <rajiv.shah01234@gmail.com>
2020-08-17 15:37:17 +0545

    create commad in termain

--- extension.js
+++ extension.js
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const path = require('path');
+const { resolve } = require('path');
+const { rejects } = require('assert');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -24,17 +27,33 @@ function activate(context) {
 		if (editor) {
 			let document = editor.document;
 			let documentText = document.getText();
-			vscode.window.activeTextEditor.selection.active.line
-			console.log(editor.selection.active.character)
-			vscode.window.showInformationMessage(documentText);
+			let line = editor.selection.active.line;
+			let texts = documentText.split('\n');
+			let command = editor.document.lineAt(line).text.split(":")[0];
+			if (!vscode.window.activeTerminal) {
+				new Promise(function (resolve, reject) {
+					let terminal = vscode.window.createTerminal()
+					resolve(terminal)
+				}).then(function (t) {
+					t.show()
+					t.sendText(`make -C ${path.dirname(editor.document.fileName)} ${command}`)
+				})
+			} else {
+				vscode.window.activeTerminal.show()
+				console.log(vscode.window.terminals)
+				vscode.window.activeTerminal.sendText(`make -C ${path.dirname(editor.document.fileName)} ${command}`)
+			}
 		}
-		// Display a message box to the user
+		//Display a message box to the user
 	});
 
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-
+function sleep(delay) {
+	var start = new Date().getTime();
+	while (new Date().getTime() < start + delay);
+}
 // this method is called when your extension is deactivated
 function deactivate() { }
 
